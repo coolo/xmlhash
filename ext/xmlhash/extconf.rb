82d63e830ce1330acf6b948dc8477567ab908c66
Stephan Kulow <coolo@suse.de>
2012-06-20 22:05:50 +0200

    use pkg-config for libxml (1.2.2)

--- ext/xmlhash/extconf.rb
+++ ext/xmlhash/extconf.rb
@@ -1,5 +1,7 @@
 require 'mkmf'
+require 'pkg-config'
 unless find_library('xml2', 'xmlAddID')
   abort "xml2 is missing.  please install libxml2"
 end
+$CFLAGS << ' ' + PackageConfig.new('libxml-2.0').cflags
 create_makefile('xmlhash/xmlhash')
