2fe4d0e00d9f5c6524bb5102821dc2675f368b54
Stephan Kulow <coolo@suse.de>
2021-12-13 20:09:50 +0100

    Add github workflow

--- ext/xmlhash/extconf.rb
+++ ext/xmlhash/extconf.rb
@@ -4,4 +4,5 @@ unless find_library('xml2', 'xmlAddID')
   abort "xml2 is missing.  please install libxml2"
 end
 $CFLAGS << ' ' + PackageConfig.new('libxml-2.0').cflags
+$CFLAGS += " -Werror "
 create_makefile('xmlhash/xmlhash')
