37bbe15c4ff2cce95a748aa95f886249d934cc7c
Stephan Kulow <coolo@suse.de>
2023-01-20 16:43:17 +0100

    Compile with -Wall

--- ext/xmlhash/extconf.rb
+++ ext/xmlhash/extconf.rb
@@ -4,5 +4,5 @@ unless find_library('xml2', 'xmlAddID')
   abort "xml2 is missing.  please install libxml2"
 end
 $CFLAGS << ' ' + PackageConfig.new('libxml-2.0').cflags
-$CFLAGS += " -Werror "
+$CFLAGS += " -Werror -Wall "
 create_makefile('xmlhash/xmlhash')
